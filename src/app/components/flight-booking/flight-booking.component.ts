fc65e1bddab2e3b5917d8250a8d29209b11fc0d3
Gaurav Kumbhare <157881474+gaurav2ook@users.noreply.github.com>
2024-11-22 19:06:09 +0530

    Update flight-booking.component.ts
    update ts with data

--- src/app/components/flight-booking/flight-booking.component.ts
+++ src/app/components/flight-booking/flight-booking.component.ts
@@ -24,14 +24,54 @@ export class FlightBookingComponent {
   flightResults = [
     {
       id: '1',
-      image: 'airline-logo.png',
-      name: 'Airline Name',
-      departure: '10:00 AM',
-      arrival: '1:00 PM',
+      image: 'assets/img/flight images/indigo.jpg',
+      name: 'IndiGo BLR to UDR',
+      departure: '8:30 AM',
+      arrival: '10:40 AM',
       type: 'Non-stop',
-      duration: '3h',
-      price: 250
+      duration: '2h 10m',
+      price: 6700
     },
+    {
+      id: '2',
+      image: 'assets/img/flight images/air india.png',
+      name: 'Air India BOM to UDR',
+      departure: '10:15 AM',
+      arrival: '11:35 AM',
+      type: 'Non-stop',
+      duration: '1h 20m',
+      price: 5200
+    },
+    {
+      id: '3',
+      image: 'assets/img/flight images/vistara.png',
+      name: 'Vistara DEL to BLR',
+      departure: '7:00 AM',
+      arrival: '9:40 AM',
+      type: 'Non-stop',
+      duration: '2h 40m',
+      price: 8500
+    },
+    {
+      id: '4',
+      image: 'assets/img/flight images/spicejet.png',
+      name: 'SpiceJet GOI to BLR',
+      departure: '5:15 PM',
+      arrival: '6:30 PM',
+      type: 'Non-stop',
+      duration: '1h 15m',
+      price: 3000
+    },
+    {
+      id: '5',
+      image: 'assets/img/flight images/go first.jpg',
+      name: 'Go First CCU to BLR',
+      departure: '11:30 AM',
+      arrival: '2:10 PM',
+      type: 'Non-stop',
+      duration: '2h 40m',
+      price: 4900
+    }
     // Additional flight data
   ];
 
