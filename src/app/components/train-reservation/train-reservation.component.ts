8204171771dd35bb283ee67ab74710fc6fc0f14b
Gaurav Kumbhare <157881474+gaurav2ook@users.noreply.github.com>
2024-11-22 19:04:25 +0530

    Update train-reservation.component.ts
    update ts with some data

--- src/app/components/train-reservation/train-reservation.component.ts
+++ src/app/components/train-reservation/train-reservation.component.ts
@@ -25,15 +25,59 @@ export class TrainReservationComponent {
   trainResults = [
     {
       id: '1',
-      image: 'train-icon.png',
-      name: 'Express Train 12345',
-      departure: '9:00 AM',
-      arrival: '3:00 PM',
-      duration: '6h',
-      stops: 'Non-stop',
+      image: 'assets/img/train images/train1.jpg',
+      name: 'CBE LTT Express',
+      departure: '4:00 PM',
+      arrival: '1:55 PM (next day)',
+      duration: '21h 55m',
+      stops: 'Limited',
       classes: 'AC 3-Tier, Sleeper',
-      price: 45
+      price: 1400
     },
+    {
+      id: '2',
+      image: 'assets/img/train images/train2.jpg',
+      name: 'Vasco Express',
+      departure: '11:05 AM',
+      arrival: '2:39 AM (next day)',
+      duration: '15h 34m',
+      stops: 'Limited',
+      classes: 'AC 2-Tier, AC 3-Tier, Sleeper',
+      price: 1250
+    },
+    {
+      id: '3',
+      image: 'assets/img/train images/rajdhani exp.jpg',
+      name: 'Rajdhani Express',
+      departure: '5:30 PM',
+      arrival: '8:35 AM (next day)',
+      duration: '15h 5m',
+      stops: 'Non-stop',
+      classes: 'AC 1-Tier, AC 2-Tier, AC 3-Tier',
+      price: 3750
+    },
+    {
+      id: '4',
+      image: 'assets/img/train images/chetak exp.jpg',
+      name: 'Chetak Express',
+      departure: '7:40 PM',
+      arrival: '8:20 AM (next day)',
+      duration: '12h 40m',
+      stops: 'Limited',
+      classes: 'AC 3-Tier, Sleeper',
+      price: 1150
+    },
+    {
+      id: '5',
+      image: 'assets/img/train images/duronto exp.jpg',
+      name: 'Duronto Express',
+      departure: '11:00 AM',
+      arrival: '6:20 PM (next day)',
+      duration: '31h 20m',
+      stops: 'Limited',
+      classes: 'AC 1-Tier, AC 2-Tier, AC 3-Tier',
+      price: 4200
+    }
   ];
 
   searchTrains() {
