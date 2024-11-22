0ef51729608e259eba4933ef2e8be22568afad1d
Gaurav Kumbhare <157881474+gaurav2ook@users.noreply.github.com>
2024-11-22 19:09:45 +0530

    Update create-hotel-package.component.ts
    update hotel package with data

--- src/app/components/adminpackage/create-hotel-package/create-hotel-package.component.ts
+++ src/app/components/adminpackage/create-hotel-package/create-hotel-package.component.ts
@@ -24,26 +24,69 @@ export class CreateHotelPackageComponent implements OnInit {
  rooms: number = 1;
  guestOptions = [1, 2, 3, 4, 5];
  roomOptions = [1, 2, 3, 4];
- hotelResults: [
+ hotelResults
+//  : [
+//   {
+//     id: string,
+//     image: string,
+//     name: string,
+//     location: string,
+//     rating: string,
+//     price: number,
+//     amenities: string
+//   }
+//  ] 
+= [
   {
-    id: string,
-    image: string,
-    name: string,
-    location: string,
-    rating: string,
-    price: number,
-    amenities: string
-  }
- ] = [
-   { 
     id: '1',
-    image: 'hotel-image.jpg',
-    name: 'Hotel Namee',
-    location: 'City Center',
-    rating: '⭐️⭐️⭐️⭐️ | 8.5/10 Excellent',
-    price: 120,
-    amenities: 'Free Wi-Fi, Pool, Gym',
-  }
+    image: 'assets/img/hotel images/leela hotel.jpg',
+    name: 'The Leela Palace Bengaluru',
+    location: 'Bengaluru',
+    rating: '⭐️⭐️⭐️⭐️ | 4.9/5 Excellent',
+    price: '₹20000' ,
+    amenities: 'Lush gardens, luxury spa, fine dining, business facilities',
+  },
+  // Additional hotel data can be added here
+  {
+    id: '2',
+    image: 'assets/img/hotel images/taj hotel.jpg',
+    name: 'Taj Lands End',
+    location: 'Mumbai',
+    rating: '⭐️⭐️⭐️⭐️ |  4.8/5 Excellent',
+    price: "₹15,000–₹20,000 per night",
+    amenities: 'Sea views, outdoor pool, spa, multiple restaurants',
+  },
+
+  {
+    id: '3',
+    image: 'assets/img/hotel images/udaipur hotel1.jpg',
+    name: 'The Oberoi Udaivilas',
+    location: ' Udaipur',
+    rating: '⭐️⭐️⭐️⭐️ |  5/5 Excellent',
+    price: '₹30,000–₹50,000 per night',
+    amenities: 'Lakeside views, private pools, spa, cultural tours',
+  },
+
+  {
+    id: '4',
+    image: 'assets/img/hotel images/imperial hotel.jpg',
+    name: 'The Imperial',
+    location: 'New Delhi',
+    rating: '⭐️⭐️⭐️⭐️ | 4.8/5 Excellent',
+    price: ' ₹15,000–₹18,000 per night',
+    amenities: 'Art Deco style, gardens, historic architecture, multiple dining options',
+  },
+
+  {
+    id: '5',
+    image: 'assets/img/hotel images/goa hotel1.jpg',
+    name: 'Fairfield by Marriott Goa Benaulim',
+    location: ' Goa',
+    rating: '⭐️⭐️⭐️⭐️ | 4.5/5 Excellent',
+    price: '₹8,000–₹12,000 per night',
+    amenities: 'Beach access, buffet dining, pool​',
+  },
+  
  ];
 
   email: string = '';
